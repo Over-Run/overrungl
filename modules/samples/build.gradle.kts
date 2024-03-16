a2b45960810e46877b0ce384d4e4f59940463877
squid233 <60126026+squid233@users.noreply.github.com>
2024-03-16 15:38:46 +0800

    Update build.gradle.kts

--- modules/samples/build.gradle.kts
+++ modules/samples/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     ).forEach {
         implementation(project(":$it"))
     }
-    testImplementation("io.github.over-run:timer:0.3.0")
+    implementation("io.github.over-run:timer:0.3.0")
     jmh("org.openjdk.jmh:jmh-core:1.37")
     jmhAnnotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.37")
 }
