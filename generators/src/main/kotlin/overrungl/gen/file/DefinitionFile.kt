a8c6d7ef717073ade731df2f510963c822c6ce88
squid233 <60126026+squid233@users.noreply.github.com>
2025-03-21 17:37:03 +0800

    feat(impl): remove zero-length determination in struct ::ofNative

--- generators/src/main/kotlin/overrungl/gen/file/DefinitionFile.kt
+++ generators/src/main/kotlin/overrungl/gen/file/DefinitionFile.kt
@@ -347,7 +347,7 @@ class DefinitionFile(filename: String? = null, rawSourceString: String? = null) 
                 |    /// Reinterprets the segment if zero-length.
                 |    /// @param segment the memory segment
                 |    /// @return the created instance or `null` if the segment is `NULL`
-                |    public static $className ofNative(MemorySegment segment) { return MemoryUtil.isNullPointer(segment) ? null : new $className(segment.byteSize() == 0 ? segment.reinterpret(LAYOUT.byteSize()) : segment); }
+                |    public static $className ofNative(MemorySegment segment) { return MemoryUtil.isNullPointer(segment) ? null : new $className(segment.reinterpret(LAYOUT.byteSize())); }
                 |
                 |    /// Creates `$className` with the given segment.
                 |    ///
@@ -355,7 +355,7 @@ class DefinitionFile(filename: String? = null, rawSourceString: String? = null) 
                 |    /// @param segment the memory segment
                 |    /// @param count   the count of the buffer
                 |    /// @return the created instance or `null` if the segment is `NULL`
-                |    public static Buffer ofNative(MemorySegment segment, long count) { return MemoryUtil.isNullPointer(segment) ? null : new Buffer(segment.byteSize() == 0 ? segment.reinterpret(LAYOUT.scale(0, count)) : segment, count); }
+                |    public static Buffer ofNative(MemorySegment segment, long count) { return MemoryUtil.isNullPointer(segment) ? null : new Buffer(segment.reinterpret(LAYOUT.scale(0, count)), count); }
                 |
                 |    /// Allocates a `$className` with the given segment allocator.
                 |    /// @param allocator the segment allocator
