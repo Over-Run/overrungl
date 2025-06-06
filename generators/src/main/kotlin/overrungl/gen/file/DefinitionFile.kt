70fb177ec029cf4871a5e35aa59dadb1de73c8e9
squid233 <60126026+squid233@users.noreply.github.com>
2025-06-06 17:11:33 +0800

    feat: add Addressable overload

--- generators/src/main/kotlin/overrungl/gen/file/DefinitionFile.kt
+++ generators/src/main/kotlin/overrungl/gen/file/DefinitionFile.kt
@@ -884,7 +884,8 @@ fun writeFunction(
     sb: StringBuilder,
     func: DefinitionFunction,
     handlesInstance: String = "Handles.get()",
-    staticMethod: Boolean = true
+    staticMethod: Boolean = true,
+    overload: Boolean = false
 ) {
     val hasDynamicType = func.returnType is DynamicValueType ||
         func.parameters.any { it.memoryLayoutWithDimensions is DefTypeDynamicValueLayout }
@@ -903,20 +904,29 @@ fun writeFunction(
             |    /// ```
         """.trimMargin()
     )
+
     sb.append("    public ")
     if (staticMethod) {
         sb.append("static ")
     }
-    sb.appendLine(
-        "${func.returnType.javaType} ${func.name}(${
-            if (func.requireAllocator) "SegmentAllocator __allocator${if (func.parameters.isNotEmpty()) ", " else ""}"
-            else ""
-        }${
-            func.parameters.joinToString { p -> "${if (p.dimensions.isNotEmpty()) "MemorySegment" else p.type.javaType} ${p.name}" }
-        }) {")
+    sb.append("${func.returnType.javaType} ${func.name}(")
+    if (func.requireAllocator) {
+        sb.append("SegmentAllocator __allocator")
+        if (func.parameters.isNotEmpty()) {
+            sb.append(", ")
+        }
+    }
+    sb.append(func.parameters.joinToString { p ->
+        "${
+            if (p.dimensions.isNotEmpty()) (if (overload) "Addressable" else "MemorySegment")
+            else (if (overload && p.type.javaType == "MemorySegment") "Addressable" else p.type.javaType)
+        } ${p.name}"
+    })
+    sb.appendLine(") {")
+
     if (func.body != null) {
         sb.appendLine(func.body.prependIndent("        "))
-    } else {
+    } else if (!overload) {
         if (func.optional) {
             sb.appendLine("""        if (MemoryUtil.isNullPointer($handlesInstance.PFN_${func.entrypoint})) throw new SymbolNotFoundError("Symbol not found: ${func.entrypoint}");""")
         }
@@ -943,7 +953,31 @@ fun writeFunction(
         }))
         sb.appendLine("; }")
         sb.appendLine("""        catch (Throwable e) { throw new RuntimeException("error in ${func.name}", e); }""")
+    } else {
+        // Addressable overload
+        sb.append("        ")
+        if (func.returnType !is VoidType) {
+            sb.append("return ")
+        }
+        sb.append("${func.name}(")
+        if (func.requireAllocator) {
+            sb.append("__allocator")
+            if (func.parameters.isNotEmpty()) {
+                sb.append(", ")
+            }
+        }
+        sb.append(func.parameters.joinToString { p ->
+            if (p.dimensions.isNotEmpty() || p.type.javaType == "MemorySegment")
+                "${p.name} != null ? ${p.name}.segment() : MemorySegment.NULL"
+            else p.name
+        })
+        sb.appendLine(");")
     }
+
     sb.appendLine("    }")
     sb.appendLine()
+
+    if (!overload && func.parameters.any { p -> p.type.javaType == "MemorySegment" }) {
+        writeFunction(sb, func, handlesInstance, staticMethod, true)
+    }
 }
