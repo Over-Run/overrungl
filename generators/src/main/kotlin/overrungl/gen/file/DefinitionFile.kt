99a31c406bf74a9190eaf82182387aebb7c9d862
squid233 <60126026+squid233@users.noreply.github.com>
2025-06-06 14:16:39 +0800

    feat: add allocInit to struct

--- generators/src/main/kotlin/overrungl/gen/file/DefinitionFile.kt
+++ generators/src/main/kotlin/overrungl/gen/file/DefinitionFile.kt
@@ -387,6 +387,31 @@ class DefinitionFile(filename: String? = null, rawSourceString: String? = null) 
             """.trimMargin()
         )
 
+        // initializer list
+        for (n in groupClass.members.size downTo 1) {
+            sb.appendLine("    /// Allocates a `$className` with the given segment allocator and arguments like initializer list.")
+            sb.appendLine("    /// @param allocator the segment allocator")
+            for (i in 0 until n) {
+                val member = groupClass.members[i]
+                sb.appendLine("    /// @param ${member.pair.name} `${member.pair.name}`")
+            }
+            sb.appendLine("    /// @return the allocated `$className`")
+            sb.append("    public static $className allocInit(SegmentAllocator allocator")
+            for (i in 0 until n) {
+                val member = groupClass.members[i]
+                sb.append(", ${member.pair.type.javaType} ${member.pair.name}")
+            }
+            sb.appendLine(") {")
+            sb.append("        return alloc(allocator)")
+            for (i in 0 until n) {
+                val member = groupClass.members[i]
+                sb.append(".${member.pair.name}(${member.pair.name})")
+            }
+            sb.appendLine(";")
+            sb.appendLine("    }")
+            sb.appendLine()
+        }
+
         sb.appendLine(
             """
                 |    /// Copies from the given source.
